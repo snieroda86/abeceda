360621cb2e737f84ca363e331006fbc3fd19c7c8
Sebastian Nieroda <snieroda86@gmail.com>
2024-06-19 16:32:23 +0200

    Featured slider carousle done

--- front-page.php
+++ front-page.php
@@ -14,8 +14,8 @@ get_header();
 <main id="primary" class="site-main">
     
         <?php  get_template_part('template-parts/home/main-slider'); ?>
-        <?php  // get_template_part('template-parts/home/features-bar'); ?>
         <?php  get_template_part('template-parts/home/recent-products-slider'); ?>
+        <?php  get_template_part('template-parts/home/recommended-products-slider'); ?>
         <?php  // get_template_part('template-parts/home/about'); ?>
         <?php  // get_template_part('template-parts/home/recommended-products-slider'); ?>
         <?php  // get_template_part('template-parts/home/bestsellers'); ?>
@@ -93,53 +93,62 @@ get_header();
         });
 
 
-    //     // Bestsellers - bestsellers-products-sn products
-    //     $('.bestsellers-products-sn').slick({
-    //       dots: true,
-    //       infinite: false,
-    //       speed: 300,
-    //       slidesToShow: 4,
-    //       slidesToScroll: 1,
-    //       prevArrow: $('.bestseller-prev'),
-    //       nextArrow: $('.bestseller-next'), 
-    //       responsive: [
-    //         {
-    //           breakpoint: 1366,
-    //           settings: {
-    //             slidesToShow: 4,
-    //             slidesToScroll: 1,
-    //             infinite: true,
-    //             dots: true,
-    //             arrows: false
-    //           }
-    //         },
-    //         {
-    //           breakpoint: 1024,
-    //           settings: {
-    //             slidesToShow: 3,
-    //             slidesToScroll: 1,
-    //             infinite: true,
-    //             dots: true,
-    //             arrows: false
-    //           }
-    //         },
-    //         {
-    //           breakpoint: 760,
-    //           settings: {
-    //             slidesToShow: 2,
-    //             slidesToScroll: 1
-    //           }
-    //         },
-    //         {
-    //           breakpoint: 480,
-    //           settings: {
-    //             slidesToShow: 1,
-    //             slidesToScroll: 1
-    //           }
-    //         }
+       // Recommended ( featured )  products
+       $('.recommended-products-sn').slick({
+          dots: false,
+          infinite: true,
+          arrows:true,
+          speed: 300,
+          slidesToShow: 4,
+          slidesToScroll: 1,
+          prevArrow: $('.ftr-prev'),
+          nextArrow: $('.ftr-next'),
+          responsive: [
+            {
+              breakpoint: 1366,
+              settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                arrows: false
+              }
+            },
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+                arrows: false
+              }
+            },
+            {
+              breakpoint: 760,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true,
+                arrows: false
+              }
+            }
             
-    //       ]
-    //     });
+          ]
+        });
+
+
+
+        
     });
 </script>
 <?php
